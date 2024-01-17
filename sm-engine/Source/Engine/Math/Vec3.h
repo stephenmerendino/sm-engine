f42af692f9f80291edd3506edd8a984714dbe500
Stephen Merendino <StephenMerendino@gmail.com>
2024-01-16 22:08:46 -0600

    Refactored Vec3

--- sm-engine/Source/Engine/Math/Vec3.h
+++ sm-engine/Source/Engine/Math/Vec3.h
@@ -1,196 +1,184 @@
 #pragma once
 #pragma warning(disable:4201)
 
-#include <engine/math/vec2.h>
+#include <Engine/math/Vec2.h>
 
-struct vec3
+class Vec3
 {
-	inline vec3() {}
+public:
+	inline Vec3();
+	inline Vec3(F32 inX, F32 inY, F32 inZ);
+
+	inline Vec3 operator*(F32 s) const;
+	inline Vec3 operator/(F32 s) const;
+	inline Vec3& operator*=(F32 s);
+	inline Vec3& operator/=(F32 s);
+	inline Vec3 operator+(const Vec3& other) const;
+	inline Vec3& operator+=(const Vec3& other);
+	inline Vec3 operator-(const Vec3& other) const;
+	inline Vec3& operator-=(const Vec3& other);
+	inline Vec3 operator-() const;
+	inline bool operator==(const Vec3& other);
+
+	inline F32 CalcLengthSq() const;
+	inline F32 CalcLength() const;
+	inline void Normalize();
+	inline Vec3 Normalized() const;
 
 	union
 	{
 		struct
 		{
-			f32 x;
-			f32 y;
-			f32 z;
+			F32 x;
+			F32 y;
+			F32 z;
 		};
 
-		struct
-		{
-			vec2 xy;
-			f32 pad0;
-		};
-
-		f32 data[3];
+		F32 data[3];
 	};
+
+	static const Vec3 ZERO;
+	static const Vec3 FORWARD;
+	static const Vec3 BACKWARD;
+	static const Vec3 UP;
+	static const Vec3 DOWN;
+	static const Vec3 LEFT;
+	static const Vec3 RIGHT;
 };
 
-inline
-vec3 make_vec3(f32 x, f32 y, f32 z)
+inline Vec3::Vec3()
+	:x(0)
+	,y(0)
+	,z(0)
 {
-	vec3 v;
-	v.x = x;
-	v.y = y;
-	v.z = z;
-	return v;
 }
 
-inline
-vec3 make_vec3(const vec2& xy, f32 z)
+inline Vec3::Vec3(F32 inX, F32 inY, F32 inZ)
+	:x(inX)
+	,y(inY)
+	,z(inZ)
 {
-	vec3 v;
-	v.xy = xy;
-	v.z = z;
-	return v;
 }
 
-inline
-vec3 operator*(const vec3& v, f32 s)
+inline Vec3 Vec3::operator*(F32 s) const
 {
-	return make_vec3(v.x * s, v.y * s, v.z * s);
+	return Vec3(x * s, y * s, z * s);
 }
 
-inline
-vec3 operator/(const vec3& v, f32 s)
+inline Vec3 Vec3::operator/(F32 s) const
 {
-	f32 inv_s = 1.0f / s;
-	return make_vec3(v.x * inv_s, v.y * inv_s, v.z * inv_s);
+	F32 invS = 1.0f / s;
+	return Vec3(x * invS, y * invS, z * invS);
 }
 
-inline
-vec3& operator*=(vec3& v, f32 s)
+inline Vec3& Vec3::operator*=(F32 s)
 {
-	v.x *= s;
-	v.y *= s;
-	v.z *= s;
-	return v;
+	x *= s;
+	y *= s;
+	z *= s;
+	return *this;
 }
 
-inline
-vec3& operator/=(vec3& v, f32 s)
+inline Vec3& Vec3::operator/=(F32 s)
 {
-	f32 inv_s = 1.0f / s;
-	v.x *= inv_s;
-	v.y *= inv_s;
-	v.z *= inv_s;
-	return v;
+	F32 invS = 1.0f / s;
+	x *= invS;
+	y *= invS;
+	z *= invS;
+	return *this;
 }
 
-inline
-vec3 operator+(const vec3& a, const vec3& b)
+inline Vec3 Vec3::operator+(const Vec3& other) const
 {
-	return make_vec3(a.x + b.x, a.y + b.y, a.z + b.z);
+	return Vec3(x + other.x, y + other.y, z + other.z);
 }
 
-inline
-vec3& operator+=(vec3& v, const vec3& add)
+inline Vec3& Vec3::operator+=(const Vec3& other)
 {
-	v.x += add.x;
-	v.y += add.y;
-	v.z += add.z;
-	return v;
+	x += other.x;
+	y += other.y;
+	z += other.z;
+	return *this;
 }
 
-inline
-vec3 operator-(const vec3& a, const vec3& b)
+inline Vec3 Vec3::operator-(const Vec3& other) const
 {
-	return make_vec3(a.x - b.x, a.y - b.y, a.z - b.z);
+	return Vec3(x - other.x, y - other.y, z - other.z);
 }
 
-inline
-vec3& operator-=(vec3& v, const vec3& sub)
+inline Vec3& Vec3::operator-=(const Vec3& other)
 {
-	v.x -= sub.x;
-	v.y -= sub.y;
-	v.z -= sub.z;
-	return v;
+	x -= other.x;
+	y -= other.y;
+	z -= other.z;
+	return *this;
 }
 
-inline
-vec3 operator-(const vec3& v)
+inline Vec3 Vec3::operator-() const
 {
-	return make_vec3(-v.x, -v.y, -v.z);
+	return Vec3(-x, -y, -z);
 }
 
-inline
-bool operator==(const vec3& a, const vec3& b)
+inline bool Vec3::operator==(const Vec3& other)
 {
-	return (a.x == b.x) && (a.y == b.y) && (a.z == b.z);
+	return (x == other.x) && (y == other.y) && (z == other.z);
 }
 
-inline
-f32 calc_length_sq(const vec3& v)
+inline F32 Vec3::CalcLengthSq() const
 {
-	return (v.x * v.x) + (v.y * v.y) + (v.z * v.z);
+	return (x * x) + (y * y) + (z * z);
 }
 
-inline
-f32 calc_length(const vec3& v)
+inline F32 Vec3::CalcLength() const
 {
-	return sqrtf(calc_length_sq(v));
+	return sqrtf(CalcLengthSq());
 }
 
-inline
-void normalize(vec3& v)
+inline void Vec3::Normalize()
 {
 	// prevent division by zero by checking for zero length and asserting
-	f32 length_sq = calc_length_sq(v);
-	SM_ASSERT(!is_zero(length_sq));
+	F32 lengthSq = CalcLengthSq();
+	SM_ASSERT(!IsZero(lengthSq));
 
 	// do normalization now that we know length is not zero
-	f32 length = sqrtf(length_sq);
-	f32 inv_length = 1.0f / length;
+	F32 length = sqrtf(lengthSq);
+	F32 invLength = 1.0f / length;
 
-	v *= inv_length;
+	*this *= invLength;
 }
 
-inline
-vec3 get_normalized(const vec3& v)
+inline Vec3 Vec3::Normalized() const
 {
-	vec3 copy = v;
-	normalize(copy);
+	Vec3 copy = *this;
+	copy.Normalize();
 	return copy;
 }
 
-inline
-vec3 operator*(f32 s, const vec3& v)
+inline Vec3 operator*(F32 s, const Vec3& v)
 {
 	return v * s;
 }
 
-inline
-f32 dot(const vec3& a, const vec3& b)
+inline F32 Dot(const Vec3& a, const Vec3& b)
 {
 	return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
 }
 
-inline
-vec3 cross(const vec3& a, const vec3& b)
+inline Vec3 Cross(const Vec3& a, const Vec3& b)
 {
-	vec3 cross;
+	Vec3 cross;
 	cross.x = a.y * b.z - a.z * b.y;
 	cross.y = a.z * b.x - a.x * b.z;
 	cross.z = a.x * b.y - a.y * b.x;
 	return cross;
 }
 
-inline
-f32 distance(const vec3& a, const vec3& b)
+inline F32 distance(const Vec3& a, const Vec3& b)
 {
-	return calc_length(a - b);
+	return (a - b).CalcLength();
 }
 
-inline
-f32 DistanceSquared(const vec3& a, const vec3& b)
+inline F32 DistanceSquared(const Vec3& a, const Vec3& b)
 {
-	return calc_length_sq(a - b);
+	return (a - b).CalcLengthSq();
 }
-
-static const vec3 VEC3_ZERO = make_vec3(0.0f, 0.0f, 0.0f);
-static const vec3 VEC3_FORWARD = make_vec3(1.0f, 0.0f, 0.0f);
-static const vec3 VEC3_BACKWARD = make_vec3(-1.0f, 0.0f, 0.0f);
-static const vec3 VEC3_UP = make_vec3(0.0f, 0.0f, 1.0f);
-static const vec3 VEC3_DOWN = make_vec3(0.0f, 0.0f, -1.0f);
-static const vec3 VEC3_LEFT = make_vec3(0.0f, 1.0f, 0.0f);
-static const vec3 VEC3_RIGHT = make_vec3(0.0f, -1.0f, 0.0f);
