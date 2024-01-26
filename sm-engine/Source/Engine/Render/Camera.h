bc32a0a99ec755d1babb41b40bf3e5f60c35906a
Stephen Merendino <StephenMerendino@gmail.com>
2024-01-26 12:24:35 -0600

    Added camera class

---
+++ sm-engine/Source/Engine/Render/Camera.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Engine/Math/Vec3.h"
+#include "Engine/Math/Mat44.h"
+
+class Camera
+{
+public:
+	Camera();
+
+	void Update(F32 ds);
+	Vec3 GetForward() const;
+	Vec3 GetRight() const;
+	Vec3 GetUp() const;
+	Mat44 GetRotation() const;
+	Mat44 GetViewTransform() const;
+	void LookAt(const Vec3& lookAtPos, const Vec3& upRef = Vec3::UP);
+
+	Vec3 m_worldPos;
+	F32 m_worldYawDeg;
+	F32 m_worldPitchDeg;
+};
