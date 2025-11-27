5b952a42deca8854e02114fbddb7d31d0183e054
Stephen Merendino <StephenMerendino@gmail.com>
2025-11-26 21:44:17 -0600

    Got batch script working for generating compile_commands.json

--- GenCompileCommandsJson.bat
+++ GenCompileCommandsJson.bat
@@ -1,4 +1,41 @@
 @echo off
 
+setlocal EnableDelayedExpansion
+
 set ScriptDir=%~dp0
 set CodeRootDir=Src
+set OutputFile=%ScriptDir%compile_commands.json
+
+set Directory=%CD%
+set "Directory=!Directory:\=/!"
+
+echo [ > %OutputFile%
+
+set "FoundFile="
+for /r "%ScriptDir%" %%f in (*.cpp *.c) do (
+    if not "!FoundFile!"=="" (
+        echo Found file: !FoundFile!
+
+        echo   { >> %OutputFile%
+        echo     "directory": "%Directory%", >> %OutputFile%
+        echo     "command": "cl.exe /ISrc /nologo !FoundFile!", >> %OutputFile%
+        echo     "file": "!FoundFile!" >> %OutputFile%
+        echo   }, >> %OutputFile%
+    )
+
+    set "FoundFile=%%f"
+    set "FoundFile=!FoundFile:%CD%\=!"
+    set "FoundFile=!FoundFile:\=/!"
+)
+
+if not "!FoundFile!"=="" (
+    echo Found file: !FoundFile!
+
+    echo   { >> %OutputFile%
+    echo     "directory": "%Directory%", >> %OutputFile%
+    echo     "command": "cl.exe /ISrc /nologo !FoundFile!", >> %OutputFile%
+    echo     "file": "!FoundFile!" >> %OutputFile%
+    echo   } >> %OutputFile%
+)
+
+echo ] >> %OutputFile%
